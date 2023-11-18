e14120ee96dadc3a50949f694086d29af0b03756
ythy <847805259@qq.com>
2023-11-18 15:17:06 +0800

    文档和图片

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ print('Python %s on %s' % (sys.version, sys.platform))
 sys.path.extend(['E:\\DeepLearn\\NetModel', 'E:\\DeepLearn\\NetModel\\SequenceModel', 'E:\\DeepLearn\\NetModel\\NGrams',
                  'E:\\DeepLearn\\NetModel\\RNN', 'E:\\DeepLearn\\NetModel\\CIFAR-10',
                  'E:\\DeepLearn\\NetModel\\FineTuning', 'E:\\DeepLearn\\NetModel\\ImageNet-Dogs',
-                 'E:\\DeepLearn\\NetModel\\NiN', 'E:\\DeepLearn\\NetModel\\GooLeNet', 'E:\\DeepLearn\\NetModel\\ResNet',
+                 'E:\\DeepLearn\\NetModel\\NiN', 'E:\\DeepLearn\\NetModel\\GoogLeNet', 'E:\\DeepLearn\\NetModel\\ResNet',
                  'E:\\DeepLearn\\NetModel\\DesnseNet', 'E:\\DeepLearn\\NetModel\\AlexNet',
                  'E:\\DeepLearn\\NetModel\\LeNet', 'E:\\DeepLearn\\NetModel\\VGG', 'E:\\DeepLearn\\NetModel\\Seq2Seq',
                  'E:\\DeepLearn\\NetModel\\Attention', 'E:/DeepLearn/NetModel'])
